d5093206935a4ea21f51de5fedfd3e49a84f6735
25 macamp <rateliasudaisuki@gmail.com>
2019-01-28 23:45:31 +0900

    #3 Add signup_path to routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   
   devise_scope :user do
     get 'signup', to: 'users/registrations#new'
+    post 'signup', to: 'users/registrations#create'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
