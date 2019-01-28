fabd09c2020b2b7fafc7001ef637834a4f67d127
25 macamp <rateliasudaisuki@gmail.com>
2019-01-28 11:22:58 +0900

    #1 Change application config
    ジェネレータの設定を変更
    不要なspecファイルを生成しないように変更

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,13 @@ module LoveKitchen
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework :rspec,
+      fixtures: false,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false,
+      controller_specs: false
+    end
   end
 end
