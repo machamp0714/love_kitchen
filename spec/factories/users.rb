b97cb9ecb2101f089bdc18488f5b34278b2da1d8
25 macamp <rateliasudaisuki@gmail.com>
2019-01-28 23:58:59 +0900

    #3 Add validation test for a valid user
    属性が有効である場合のテストを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
-  factory :user do
-    
+  factory :alice, class: User do
+    name { 'alice' }
+    email { 'alice@email.com' }
+    password { 'password' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { FactoryBot.create(:alice) }
+
+  describe 'ユーザーの有効性について' do
+    context '有効な情報の場合' do
+      it '有効であること' do
+        expect(user).to be_valid
+      end
+    end
+  end
 end
