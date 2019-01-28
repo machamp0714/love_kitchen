c49231d6148638b9747cbeb735c814f147ff3f40
25 macamp <rateliasudaisuki@gmail.com>
2019-01-28 23:41:45 +0900

    #3 Change routes
    deviseをカスタマイズして独自のパスを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root 'pages#home'
+  
+  devise_scope :user do
+    get 'signup', to: 'users/registrations#new'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
