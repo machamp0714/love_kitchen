0901d091936b250bea20bc5827af3516999fde7c
25 macamp <rateliasudaisuki@gmail.com>
2019-01-30 15:28:36 +0900

    #3 Add a new request spec
    ログイン状態の時、トップページへリダイレクトすることをテスト

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+
+  let(:user) { FactoryBot.create(:bob) }
+
   describe 'GET #new' do
-    it "works! (now write some real specs)" do
+    it "ログインしている時、トップページにリダイレクトすること" do
+      sign_in user
       get signup_path
-      expect(response).to have_http_status(200)
+      expect(response).to redirect_to root_url
     end
   end
 end
