229b157eac9b7406607c429f6d36c6c5b20b84b5
25 macamp <rateliasudaisuki@gmail.com>
2019-01-30 15:56:51 +0900

    #3 Add a new feature spec
    新しいユーザーを作成できることをテスト

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.feature "Users", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  scenario '新しいユーザーを作成できること' do
+    visit root_path
+    click_link "無料会員登録"
+    expect {
+      fill_in "ユーザー名", with: "alice"
+      fill_in "メールアドレス", with: "alice@email.com"
+      fill_in "パスワード", with: "password"
+      fill_in "確認用パスワード", with: "password"
+      click_on "登録する"
+    }.to change(User, :count).by(1)  
+  end
 end
