79d5cfdf7cbd29dabca0cd43dc3479da60ab7280
25 macamp <rateliasudaisuki@gmail.com>
2019-01-31 15:37:26 +0900

    #5 Add login_path to rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'signup', to: 'users/registrations#new'
     post 'signup', to: 'users/registrations#create'
+    get 'login', to: 'users/sessions#new'
+    post 'login', to: 'users/sessions#create'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
