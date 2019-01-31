fb9d7deb5c9a1bae7c5ceeeb306b8fb3778cf1aa
25 macamp <rateliasudaisuki@gmail.com>
2019-01-31 16:03:38 +0900

    #5 Complete login scenario
    ログイン出来ることをテスト
    green

--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -1,12 +1,17 @@
 require 'rails_helper'
 
 RSpec.feature "Sessions", type: :feature do
+
+  before do
+    FactoryBot.create(:alice)
+  end
+
   scenario 'ログイン出来ること' do
     visit root_path
     click_link 'ログイン'
     fill_in 'メールアドレス', with: 'alice@email.com'
     fill_in 'パスワード', with: 'password'
-    click_on 'ログイン'
-    expect(page).to have_content 'ログアウト'
+    click_on 'ログインする'
+    expect(page).to have_content 'ログインしました'
   end
 end
