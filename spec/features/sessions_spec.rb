e594e199e7f0563134cddf87ca4dea0f6755c805
25 macamp <rateliasudaisuki@gmail.com>
2019-01-31 15:45:23 +0900

    #5 Add a new scenario
    ログイン出来ることに対するテストを追加
    まだred

--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.feature "Sessions", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  scenario 'ログイン出来ること' do
+    visit root_path
+    click_link 'ログイン'
+    fill_in 'メールアドレス', with: 'alice@email.com'
+    fill_in 'パスワード', with: 'password'
+    click_on 'ログイン'
+    expect(page).to have_content 'ログアウト'
+  end
 end
