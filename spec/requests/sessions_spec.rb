c924ef3242445d5ecde01a2dd24822c9520ef1ef
25 macamp <rateliasudaisuki@gmail.com>
2019-01-31 17:00:56 +0900

    #5 Update sessions_request_spec
    不要なテストを削除

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -2,13 +2,4 @@ require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
 
-  let(:user) { FactoryBot.create(:alice) }
-
-  describe 'POST #create' do
-    it "ログインするとトップページにリダイレクトすること" do
-      sign_in user
-      delete logout_path
-      expect(response).to redirect_to root_url
-    end
-  end
 end
