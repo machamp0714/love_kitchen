b01b1e676f763f30736dcf5361fbaa863cef49ec
25 macamp <rateliasudaisuki@gmail.com>
2019-01-31 16:53:45 +0900

    #5 Add a new request spec

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
-  describe "GET /sessions" do
-    it "works! (now write some real specs)" do
-      get sessions_index_path
-      expect(response).to have_http_status(200)
+
+  let(:user) { FactoryBot.create(:alice) }
+
+  describe 'POST #create' do
+    it "ログインするとトップページにリダイレクトすること" do
+      sign_in user
+      delete logout_path
+      expect(response).to redirect_to root_url
     end
   end
 end
