8b42c0b1ac446b3c39c33931930ddebbf302a07e
25 macamp <rateliasudaisuki@gmail.com>
2019-02-03 20:17:46 +0900

    #7 Change carrierwave default_url
    画像がアップロードされなかった場合、default.pngが表示されるように変更

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -14,10 +14,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-      ActionController::Base.helpers.asset_path("fallback/" + [version_name, "profile.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+    'default.png'
   end
 
   # Process files as they are uploaded:
