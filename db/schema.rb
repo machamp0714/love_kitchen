6ff690f965328f71846f2f03a49c559c74e491b9
25 macamp <rateliasudaisuki@gmail.com>
2019-02-03 16:47:36 +0900

    7 Add avatar column to user
    usersテーブルに新しくavatar属性を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_03_065749) do
+ActiveRecord::Schema.define(version: 2019_02_03_074627) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_02_03_065749) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "introduce"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
