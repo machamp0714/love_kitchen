a4467e1122fe186cabe45fa95f2b75f104e676a4
25 macamp <rateliasudaisuki@gmail.com>
2019-02-03 17:20:05 +0900

    #7 Change avatar uploader default url

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -13,12 +13,12 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
+  def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+      ActionController::Base.helpers.asset_path("fallback/" + [version_name, "profile.png"].compact.join('_'))
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  end
 
   # Process files as they are uploaded:
   process scale: [255, 255]
