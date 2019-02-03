754eb747d4807d3a99d97ccca08702eeab8ebdc6
25 macamp <rateliasudaisuki@gmail.com>
2019-02-03 16:49:26 +0900

    #7 Add mount_uploader to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
+  mount_uploader :avatar, AvatarUploader
+
   validates :name,
     presence: true,
     length: { maximum: 12 }
