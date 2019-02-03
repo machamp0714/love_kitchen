c757e9d55eda8fb270a416822196e67027efc56e
25 macamp <rateliasudaisuki@gmail.com>
2019-02-03 21:19:30 +0900

    #7 Add setting_profile_path
    ユーザーを編集するためのパスを追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'signup', to: 'users/registrations#new'
     post 'signup', to: 'users/registrations#create'
+    get 'setting/profile', to: 'users/registrations#edit'
     get 'login', to: 'users/sessions#new'
     post 'login', to: 'users/sessions#create'
     delete 'logout', to: 'users/sessions#destroy'
