b1b6db6d63bb4d1bbf05d8b29e323c05c8029a94
25 macamp <rateliasudaisuki@gmail.com>
2019-02-04 15:42:12 +0900

    #7 Change devise registrations_controller
    update_resourceメソッドをオーバーライド
    パスワードが未記入でもプロフィールを編集出来るように変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -55,6 +55,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
     root_path
   end
 
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+  
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
