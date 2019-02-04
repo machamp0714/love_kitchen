8d6c4a4270cd69a10336862c682b9d4b5fa0428a
25 macamp <rateliasudaisuki@gmail.com>
2019-02-04 16:54:50 +0900

    #7 Fix registrations_controller strong parameter
    プロフィールを更新出来ない問題を修正
    strong parameterで新しく追加された属性の更新を許可
    scaleメソッドの代わりにresize_to_fitを使用

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
   
   # GET /resource/sign_up
   # def new
@@ -58,7 +58,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def update_resource(resource, params)
     resource.update_without_password(params)
   end
-  
+
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
