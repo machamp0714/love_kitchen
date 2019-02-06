9acbe8a009d622201fbf9e4a60601854cdef6dc8
25 macamp <rateliasudaisuki@gmail.com>
2019-02-06 21:53:10 +0900

    #7 Clean users_spec with support module
    session_supportを作成し、users_specをリファクタリング

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.feature "Users", type: :feature do
-  before do
-      FactoryBot.create(:bob)
-    end
   
+  given(:user) { FactoryBot.create(:alice) }
+
   scenario '新しいユーザーを作成できること' do
     visit root_path
     click_link "無料会員登録"
@@ -17,16 +16,12 @@ RSpec.feature "Users", type: :feature do
     }.to change(User, :count).by(1)  
     expect(page).to have_content "アカウント登録が完了しました。"
   end
+
   scenario 'プロフィールを編集出来ること' do
-    visit root_path
-    click_link "ログイン"
-    fill_in 'メールアドレス', with: "bob@email.com"
-    fill_in 'パスワード', with: "password"
-    click_on "ログインする"
+    sign_in_as user
     click_link "プロフィールを編集する"
-    fill_in "自己紹介", with: "hello, my name is bob."
+    fill_in "自己紹介", with: "hello, my name is alice."
     click_on "更新する"
     expect(page).to have_content "アカウント情報を変更しました。"
-
   end
 end
