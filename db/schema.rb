991fb45512ac2ef27a162603af6d79bb582b10d3
25 macamp <rateliasudaisuki@gmail.com>
2019-02-06 23:53:14 +0900

    #7 Add friendly_url to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_03_074627) do
+ActiveRecord::Schema.define(version: 2019_02_06_144030) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -23,7 +23,9 @@ ActiveRecord::Schema.define(version: 2019_02_03_074627) do
     t.datetime "updated_at", null: false
     t.string "introduce"
     t.string "avatar"
+    t.string "friendly_url"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["friendly_url"], name: "index_users_on_friendly_url", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
