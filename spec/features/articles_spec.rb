cf296c19936486e31f7fdbc90c1d11ff8a0f7c44
25 macamp <rateliasudaisuki@gmail.com>
2019-02-09 22:34:11 +0900

    #9 Add a new scenario to articles feature spec
    記事を投稿できることに対するテストを追加
    red
    greenにするためアプリケーションを実装していく

--- spec/features/articles_spec.rb
+++ spec/features/articles_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.feature "Articles", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+
+  given(:alice) { FactoryBot.create(:alice) }
+
+  scenario '記事を投稿できること' do
+    sign_in_as alice
+    click_link "投稿する"
+    expect {
+      fill_in "タイトル", with: "アルミパン"
+      fill_in "ストーリー", with: "アルミパンはコスパがいい"
+      click_on "投稿"
+    }.to change(Article, :count).by(1)
+    expect(page).to have_content "Success!!"
+  end
 end
