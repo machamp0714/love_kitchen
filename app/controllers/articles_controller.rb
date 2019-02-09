0a64dbe8f44d4fb8248eed4d19e923f075c8f56c
25 macamp <rateliasudaisuki@gmail.com>
2019-02-10 00:41:35 +0900

    #9 Add create action to articles controller
    記

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,19 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def create
+        @article = current_user.articles.build(article_params)
+        if @article.save
+            redirect_to current_user, notice: 'Success!!'
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def article_params
+        params.require(:article).permit(:title, :content)
+    end
+
 end
