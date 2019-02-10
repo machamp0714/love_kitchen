723e3606a0ed3d9be651850593b5534c0230876b
25 macamp <rateliasudaisuki@gmail.com>
2019-02-10 15:52:05 +0900

    #9 Add a new scenario to articles feature spec
    記事を編集できることに対するテストを追加
    まだred

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # devise_for :users
+  devise_for :users
   devise_scope :user do
     get 'signup', to: 'users/registrations#new'
     post 'signup', to: 'users/registrations#create'
--- spec/features/articles_spec.rb
+++ spec/features/articles_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.feature "Articles", type: :feature do
 
   given(:alice) { FactoryBot.create(:alice) }
+  before do
+    @article = FactoryBot.create(:test, user: alice)
+  end
 
   scenario '記事を投稿できること' do
     sign_in_as alice
@@ -13,5 +16,16 @@ RSpec.feature "Articles", type: :feature do
       click_on "LoveKitchenに投稿"
     }.to change(Article, :count).by(1)
     expect(page).to have_content "Success!!"
+  end
+
+  scenario '記事を編集できること' do
+    sign_in_as alice
+    click_link "プロフィール"
+    click_link @article.title
+    click_link "編集する"
+    fill_in "article[title]", with: "編集テスト"
+    fill_in "article[content", with: "テスト投稿"
+    click_on "編集する"
+    expect(page).to have_content "Updated!!"
   end
 end
