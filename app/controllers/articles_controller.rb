203e929b6cc66aa09fc68a744b40a79bd6104606
25 macamp <rateliasudaisuki@gmail.com>
2019-02-10 14:38:56 +0900

    #9 Add show action to articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,10 @@ class ArticlesController < ApplicationController
         end
     end
 
+    def show 
+        @article = Article.find(params[:id])
+    end
+
     private
 
     def article_params
