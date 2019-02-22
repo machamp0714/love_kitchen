4e4d17d987796f393b759ac5b5c332332c7db7d2
25 macamp <rateliasudaisuki@gmail.com>
2019-02-22 22:43:46 +0900

    Add all articles index to home page #23

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
   def home
+    if user_signed_in?
+      @articles = []
+      users = User.all
+      users.each do |user| 
+        @user_articles = user.articles_find
+      end
+      @user_articles.each do |article|
+        @articles << article
+      end
+    end
   end
 end
