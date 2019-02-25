df02adef8f664f3e9592ce360794681595b9d983
25 macamp <rateliasudaisuki@gmail.com>
2019-02-26 00:46:33 +0900

    Change picture uploader resize #16

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -21,7 +21,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fit: [200, 300]
+  process resize_to_fill: [386, 246]
   #
   # def scale(width, height)
   #   # do something
