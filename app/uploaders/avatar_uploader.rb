e6b4de0383bdb134246a94b8aab492c512668ee2
25 macamp <rateliasudaisuki@gmail.com>
2019-02-26 00:28:12 +0900

    Change resize_to_fit to resize_to_fill #16

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -18,7 +18,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   # Process files as they are uploaded:
-  process resize_to_fit: [255, 255]
+  process resize_to_fill: [255, 255]
   #
   # def scale(width, height)
   #   # do something
