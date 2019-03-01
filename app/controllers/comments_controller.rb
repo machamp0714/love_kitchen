9df521915ba88794587d688662246ba24f8c9177
25 macamp <rateliasudaisuki@gmail.com>
2019-03-02 00:19:05 +0900

    Create create action : comments_controller #24

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+
+    def create
+        @article = Article.find(params[:comment][:article_id])
+        @comment = current_user.comments.build(comment_params)
+        if @comment.save
+            redirect_to @article
+        else
+            :show
+        end
+    end
+
+    private
+        def comment_params
+            params.require(:comment).permit(:content, :article_id)
+        end
 end
