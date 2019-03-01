50561ac1148516e24cc968432c8c9d3e4ab1751d
25 macamp <rateliasudaisuki@gmail.com>
2019-03-01 22:27:15 +0900

    Fix comment test #24

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -3,15 +3,11 @@ require 'rails_helper'
 RSpec.feature "Comments", type: :feature do
 
   given(:user) { FactoryBot.create(:alice) }
-  given(:other_user) { FactoryBot.create(:bob) }
-  
-  before do
-    @article = FactoryBot.create(:bob_article, user: other_user)
-  end
+  given!(:article) { FactoryBot.create(:alice_article, user: user) }
 
   scenario 'コメントを投稿できること' do
     sign_in_as user
-    click_link @article.title
+    click_link article.title
     expect {
       fill_in :content, with: "コメントテスト"
       click_on "投稿"
