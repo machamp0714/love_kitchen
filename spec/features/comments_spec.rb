317ffa78e933dbf1d488bdbe9a8bf138e5b9873b
25 macamp <rateliasudaisuki@gmail.com>
2019-03-03 15:20:41 +0900

    Add a new scenario #24
    コメントを削除できることに対するテストを追加
    red

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -4,6 +4,7 @@ RSpec.feature "Comments", type: :feature do
 
   given(:user) { FactoryBot.create(:alice) }
   given!(:article) { FactoryBot.create(:alice_article, user: user) }
+  given!(:comment) { FactoryBot.create(:alice_comment, user: user, article: article) }
 
   scenario 'コメントを投稿できること' do
     sign_in_as user
@@ -12,5 +13,13 @@ RSpec.feature "Comments", type: :feature do
       fill_in 'comment[content]', with: "コメントテスト"
       click_on "投稿"
     }.to change(Comment, :count).by(1)
+  end
+
+  scenario 'コメントを削除できること' do
+    sign_in_as user
+    click_link article.title
+    expect {
+      click_link '削除'
+    }.to change(Comment, :count).by(-1)
   end
 end
