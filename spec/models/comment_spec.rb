d87d8e393be82abc6d19d4d14ec6e13ffc2faaee
25 macamp <rateliasudaisuki@gmail.com>
2019-03-03 13:18:19 +0900

    Add a new model spec #24
    コメントが空白である場合無効であることに対するテストを追加
    まだred

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { FactoryBot.create(:alice) }
+  let(:article) { FactoryBot.create(:alice_article, user: user) }
+  let(:comment) { FactoryBot.create(:alice_comment, user: user, article: article) }
+
+  it 'コメントが空白の場合無効であること' do
+    comment.content = ""
+    expect(comment).to_not be_valid
+  end
 end
