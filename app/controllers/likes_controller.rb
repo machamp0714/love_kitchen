e229b13f65df420fb161faf96bbdeb0de2767d10
25 macamp <rateliasudaisuki@gmail.com>
2019-03-05 01:03:23 +0900

    Add create action to likes_controller #25

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,11 @@
 class LikesController < ApplicationController
   def create
+    current_user.likes.create(article_id: params[:article_id])
+    @article = Article.find(params[:article_id])
+    respond_to do |format|
+      format.html { redirect_to @article }
+      format.js
+    end
   end
 
   def destroy
