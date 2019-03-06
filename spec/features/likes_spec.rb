06ebc17ede0ab72329e31b3e3049e59645eb238f
25 macamp <rateliasudaisuki@gmail.com>
2019-03-06 23:08:55 +0900

    Add feature do #25

--- spec/features/likes_spec.rb
+++ spec/features/likes_spec.rb
@@ -5,13 +5,15 @@ RSpec.feature "Likes", type: :feature do
   given(:user) { FactoryBot.create(:alice) }
   given(:other_user) { FactoryBot.create(:bob) }
   given!(:article) { FactoryBot.create(:bob_article, user: other_user) }
-
-  scenario '「いいね」できること' do
-    sign_in_as user
-    click_link article.title
-    expect {
-      find('.likable').click
-    }.to change(Like, :count).by(1)
-    expect(page).to have_css '.liked'
+  
+  feature 'まだ「いいね」していない場合' do
+    scenario '「いいね」できること' do
+      sign_in_as user
+      click_link article.title
+      expect {
+        find('.likable').click
+      }.to change(Like, :count).by(1)
+      expect(page).to have_css '.liked'
+    end
   end
 end
