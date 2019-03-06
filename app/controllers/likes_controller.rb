f852492b0aafcc212145cfdd8183005b53570103
25 macamp <rateliasudaisuki@gmail.com>
2019-03-06 22:24:27 +0900

    Create destroy action in likes_controller #25
    いいねを解除できるようにdestroyアクションを作成

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -9,5 +9,12 @@ class LikesController < ApplicationController
   end
 
   def destroy
+    @like = Like.find(params[:id])
+    @article = Article.find(@like.article_id)
+    @like.destroy
+    respond_to do |format|
+      format.html { redirect_to @article }
+      format.js
+    end
   end
 end
