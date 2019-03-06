9992efbde7640493391f68a25b2a5496cb53f65b
25 macamp <rateliasudaisuki@gmail.com>
2019-03-06 22:50:13 +0900

    Add a new scenario #25
    いいねできることに対するテストを追加
    green

--- spec/features/likes_spec.rb
+++ spec/features/likes_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.feature "Likes", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+
+  given(:user) { FactoryBot.create(:alice) }
+  given(:other_user) { FactoryBot.create(:bob) }
+  given!(:article) { FactoryBot.create(:bob_article, user: other_user) }
+
+  scenario '「いいね」できること' do
+    sign_in_as user
+    click_link article.title
+    expect {
+      find('.likable').click
+    }.to change(Like, :count).by(1)
+    expect(page).to have_css '.liked'
+  end
 end
