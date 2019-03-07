0feeca8d13a81b542eb81938c6951356e1bc5415
25 macamp <rateliasudaisuki@gmail.com>
2019-03-07 23:49:13 +0900

    Fix seeds #28

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,23 @@
 # user
 User.create!(
-    name: 'alice',
-    email: 'alice@gmail.com',
-    password: 'password',
-    password_confirmation: 'password',
-    introduce: 'hello! my name is alice.'
+    name: "alice",
+    email: "alice@gmail.com",
+    password: "password",
+    password_confirmation: "password",
+    introduce: "hello! my name is alice."
 )
 
 # articles
 user = User.first
 21.times do |i|
     user.articles.create!(
-        title: 'テストタイトル #{i+1}',
-        content: 'テスト投稿 #{i+1}',
-        label1: 'ラベル1',
-        label2: 'ラベル2',
-        label3: 'ラベル3',
-        label4: 'ラベル4',
-        label5: 'ラベル5',
+        title: "テストタイトル #{i+1}",
+        content: "テスト投稿 #{i+1}",
+        label1: "ラベル1",
+        label2: "ラベル2",
+        label3: "ラベル3",
+        label4: "ラベル4",
+        label5: "ラベル5",
         data1: 1,
         data2: 2,
         data3: 3,
