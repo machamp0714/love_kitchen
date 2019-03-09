003e6797156dc37d037158318ad1a1a0f46e27c5
25 macamp <rateliasudaisuki@gmail.com>
2019-03-09 23:33:28 +0900

    Fix likes request spec #25

--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "Likes", type: :request do
 
-  let(:like_params) { FactoryBot.attributes_for(:like) }
-
   describe 'POST #create' do
+    before do
+      @user = FactoryBot.create(:alice)
+      @article = FactoryBot.create(:alice_article, user: @user)
+      @like_params = FactoryBot.attributes_for(:like)
+    end
     it 'ログインしていないユーザーは「いいね」出来ないこと' do
-      post likes_path, params: like_params
+      post article_likes_path(@article, @user.likes.build), params: @like_params
       expect(response).to redirect_to login_url
     end
   end
