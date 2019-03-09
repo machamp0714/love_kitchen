72d60abe50bf498467f4a253abf20b2a3ef88ace
25 macamp <rateliasudaisuki@gmail.com>
2019-03-09 23:16:27 +0900

    Fix likes feature spec #25

--- spec/features/likes_spec.rb
+++ spec/features/likes_spec.rb
@@ -4,13 +4,12 @@ RSpec.feature "Likes", type: :feature do
 
   given(:user) { FactoryBot.create(:alice) }
   given(:other_user) { FactoryBot.create(:bob) }
-  given!(:alice_article) { FactoryBot.create(:alice_article, user: user) }
-  given!(:bob_article) { FactoryBot.create(:bob_article, user: other_user) }
+  given!(:article) { FactoryBot.create(:bob_article, user: other_user) }
   
   feature 'まだ「いいね」していない場合' do
     scenario '「いいね」できること' do
-      sign_in_as other_user
-      click_link alice_article.title
+      sign_in_as user
+      click_link article.title
       expect {
         find('.likable').click
       }.to change(Like, :count).by(1)
@@ -20,11 +19,11 @@ RSpec.feature "Likes", type: :feature do
 
   feature '「いいね」済みの場合' do
     background do
-      FactoryBot.create(:like, user: user, article: bob_article)
+      FactoryBot.create(:like, user: user, article: article)
     end
     scenario '「いいね」解除できること' do
       sign_in_as user
-      click_link bob_article.title
+      click_link article.title
       expect {
         find('.liked').click
       }.to change(Like, :count).by(-1)
