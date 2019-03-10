cf3d6e9a40166195f0a3c5556a5bdc9b3fc12c50
25 macamp <rateliasudaisuki@gmail.com>
2019-03-10 11:36:18 +0900

    Create favorites create action #26

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,6 +3,12 @@ class FavoritesController < ApplicationController
   end
 
   def create
+    current_user.favorites.create(article_id: params[:article_id])
+    @article = Article.find(params[:article_id])
+    respond_to do |format|
+      format.html { redirect_to @article }
+      format.js
+    end
   end
 
   def destroy
