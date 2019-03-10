47847839a352658ff0707c347f0935b69dcc9fd2
25 macamp <rateliasudaisuki@gmail.com>
2019-03-10 14:26:21 +0900

    Add a new scenario to favorites feature spec #26
    お気に入りできることに対するテストを追加

--- spec/features/favorites_spec.rb
+++ spec/features/favorites_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.feature "Favorites", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+
+  given(:user) { FactoryBot.create(:alice) }
+  given(:other_user) { FactoryBot.create(:bob) }
+  given!(:article) { FactoryBot.create(:bob_article, user: other_user) }
+
+  feature 'まだお気に入りしていない場合' do
+    scenario 'お気に入りに記事を追加できること' do
+      sign_in_as user
+      click_link article.title
+      expect {
+        find('.favorite').click
+      }.to change(Favorite, :count).by(1)
+      expect(page).to have_css '.unfavorite'
+    end
+  end
 end
