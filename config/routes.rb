ff479d2842c5411b86b814e099a43d79ad515ac4
25 macamp <rateliasudaisuki@gmail.com>
2019-03-11 09:50:22 +0900

    :tada: 開発中にメールを確認出来るよう変更 #10

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, skip: :all
-  
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   devise_scope :user do
     get 'signup', to: 'users/registrations#new'
     post 'signup', to: 'users/registrations#create'
