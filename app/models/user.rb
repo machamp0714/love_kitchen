a9f6ba0bb8733304c80d831ffb820d42240a1d26
25 macamp <rateliasudaisuki@gmail.com>
2019-03-11 14:02:45 +0900

    :sparkles: Userモデルにfollowersの関連付けを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,13 @@ class User < ApplicationRecord
     class_name: 'Relationship',
     foreign_key: 'follower_id',
     dependent: :destroy
+  has_many :passive_relationships,
+    class_name: 'Relationship',
+    foreign_key: 'followed_id',
+    dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
   validates :name,
     presence: true,
     length: { in: 3..20 },
