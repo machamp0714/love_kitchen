049b3f7995aef6001fd9e1c5eaf03727649ef0b0
25 macamp <rateliasudaisuki@gmail.com>
2019-03-11 11:43:00 +0900

    :sparkles: UserモデルとRelationshipモデルの間に関連付けを実装 #51

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,10 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :active_relationships, 
+    class_name: 'Relationship',
+    foreign_key: 'follower_id',
+    dependent: :destroy
 
   validates :name,
     presence: true,
