746b308501fbf1e7dbdebded19a71036862d26c6
25 macamp <rateliasudaisuki@gmail.com>
2019-03-11 11:34:14 +0900

    :tada: relationshipsテーブルを作成 #51
    インデックスも同時に追加

--- db/migrate/20190311023044_create_relationships.rb
+++ db/migrate/20190311023044_create_relationships.rb
@@ -6,5 +6,8 @@ class CreateRelationships < ActiveRecord::Migration[5.2]
 
       t.timestamps
     end
+    add_index :relationships, :followed_id
+    add_index :relationships, :follower_id
+    add_index :relationships, [:followed_id, :follower_id], unique: true
   end
 end
