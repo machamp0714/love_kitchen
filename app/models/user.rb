4648da69599786ab3b2bf6a7713fdfae1dbb6bb0
25 macamp <rateliasudaisuki@gmail.com>
2019-03-11 13:35:05 +0900

    :sparkles: Userモデルにfollowingの関連付けを追加 #51

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
     class_name: 'Relationship',
     foreign_key: 'follower_id',
     dependent: :destroy
-
+  has_many :following, through: :active_relationships, source: :followed
   validates :name,
     presence: true,
     length: { in: 3..20 },
