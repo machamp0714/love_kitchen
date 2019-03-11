c5760f18601a6b6ab083fdf88e2a47149eb25f44
25 macamp <rateliasudaisuki@gmail.com>
2019-03-11 15:33:41 +0900

    :tada: relationships_controllerにcreate,destroyアクションを追加 #51
    ユーザーが他のユーザーをフォロー、フォロー解除できるようになった

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,19 @@
 class RelationshipsController < ApplicationController
   def create
+    @user = User.find(params[:followed_id])
+    current_user.follow(@user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 
   def destroy
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 end
