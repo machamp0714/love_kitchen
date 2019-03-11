d9d09506006922f5ce91311f4b8fff68358ed97e
25 macamp <rateliasudaisuki@gmail.com>
2019-03-11 22:48:16 +0900

    :up: 新しいseedsを作成 #51

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,48 @@ User.create!(
     password_confirmation: "password",
     introduce: "hello! my name is alice."
 )
-
+User.create!(
+    name: "bob",
+    email: "bob@gmail.com",
+    password: "password",
+    password_confirmation: "password",
+    introduce: "hello! my name is bob."
+)
+User.create!(
+    name: "carol",
+    email: "carol@gmail.com",
+    password: "password",
+    password_confirmation: "password",
+    introduce: "hello! my name is carol."
+)
+User.create!(
+    name: "david",
+    email: "david@gmail.com",
+    password: "password",
+    password_confirmation: "password",
+    introduce: "hello! my name is david."
+)
+User.create!(
+    name: "elis",
+    email: "elis@gmail.com",
+    password: "password",
+    password_confirmation: "password",
+    introduce: "hello! my name is elis."
+)
+User.create!(
+    name: "frank",
+    email: "frank@gmail.com",
+    password: "password",
+    password_confirmation: "password",
+    introduce: "hello! my name is frank."
+)
+User.create!(
+    name: "gloria",
+    email: "gloria@gmail.com",
+    password: "password",
+    password_confirmation: "password",
+    introduce: "hello! my name is gloria."
+)
 # articles
 user = User.first
 21.times do |i|
