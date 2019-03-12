c7a2f75d2f7eee71f5f230c6a3e59bf1c4068423
25 macamp <rateliasudaisuki@gmail.com>
2019-03-13 00:01:13 +0900

    :tada: feedメソッドを実装 #51

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,6 +44,7 @@ class User < ApplicationRecord
   end
 
   def feed
-
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Article.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
   end
 end
