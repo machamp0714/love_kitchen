0db9d8f2e4538813692cb8cfb18acab0da105fa5
25 macamp <rateliasudaisuki@gmail.com>
2019-03-12 22:56:40 +0900

    :up: 新しいseedsを作成 #51
    記事のseedsを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,7 +50,7 @@ User.create!(
 )
 # articles
 user = User.first
-21.times do |i|
+5.times do |i|
     user.articles.create!(
         title: "テストタイトル #{i+1}",
         content: "テスト投稿 #{i+1}",
@@ -64,5 +64,62 @@ user = User.first
         data3: 3,
         data4: 4,
         data5: 5
+    )
+end
+
+second_user = User.find(2)
+5.times do |i|
+    second_user.articles.create!(
+        title: "テストタイトル #{i+1}",
+        content: "テスト投稿 #{i+1}",
+        label1: "ラベル1",
+        label2: "ラベル2",
+        label3: "ラベル3",
+        label4: "ラベル4",
+        label5: "ラベル5",
+        data1: 1,
+        data2: 2,
+        data3: 3,
+        data4: 4,
+        data5: 5,
+        created_at: 1.days.ago
+    )
+end
+
+third_user = User.find(3)
+5.times do |i|
+    third_user.articles.create!(
+        title: "テストタイトル #{i+1}",
+        content: "テスト投稿 #{i+1}",
+        label1: "ラベル1",
+        label2: "ラベル2",
+        label3: "ラベル3",
+        label4: "ラベル4",
+        label5: "ラベル5",
+        data1: 1,
+        data2: 2,
+        data3: 3,
+        data4: 4,
+        data5: 5,
+        created_at: 1.weeks.ago
+    )
+end
+
+fourth_user = User.find(4)
+5.times do |i|
+    fourth_user.articles.create!(
+        title: "テストタイトル #{i+1}",
+        content: "テスト投稿 #{i+1}",
+        label1: "ラベル1",
+        label2: "ラベル2",
+        label3: "ラベル3",
+        label4: "ラベル4",
+        label5: "ラベル5",
+        data1: 1,
+        data2: 2,
+        data3: 3,
+        data4: 4,
+        data5: 5,
+        created_at: 1.years.ago
     )
 end
