99639514011760fe03247468df970705d188ea64
25 macamp <rateliasudaisuki@gmail.com>
2019-03-15 23:08:29 +0900

    :sparkles: application_controlleraにset_searchフィルタを追加 #55
    ナビゲーションに検索機能を実装するため

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
+    before_action :set_search
+
     private
         def own_article?
             @article = Article.find(params[:article_id])
             redirect_to @article if current_user.id == @article.user_id
+        end
+
+        def set_search
+            @q = Article.ransack(params[:q])
+            @search_articles = @q.result(distinct: true)
         end
 end
