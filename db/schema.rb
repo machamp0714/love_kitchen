7d2ed932eac935160938f9e34853485c289d33d7
25 macamp <rateliasudaisuki@gmail.com>
2019-03-18 22:12:06 +0900

    :+1: name属性に一意性を強制するためのmigrationを追加 #3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_11_023044) do
+ActiveRecord::Schema.define(version: 2019_03_18_131104) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(version: 2019_03_11_023044) do
     t.string "avatar"
     t.string "slug"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_users_on_slug", unique: true
   end
