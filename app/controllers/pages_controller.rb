043b8e2311fdf304caac121bd06fb22c9fce1e67
25 macamp <rateliasudaisuki@gmail.com>
2019-03-20 00:15:35 +0900

    :+1: pages_controlleraを修正 #60
    トップページで20件の投稿を見ることが出来るようにインスタンス変数を定義
    if分岐が不要になったため削除

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,13 @@
 class PagesController < ApplicationController
   def home
-    if user_signed_in?
-      articles = []
-      User.joins(:articles).includes(:articles).order("articles.created_at DESC").each do |user|
-        user.articles.each do |article|
-          articles << article
-        end
+    articles = []
+    User.joins(:articles).includes(:articles).order("articles.created_at DESC").each do |user|
+      user.articles.each do |article|
+        articles << article
       end
-      @articles = Kaminari.paginate_array(articles).page(params[:page]).per(10)
     end
+    @articles = Kaminari.paginate_array(articles).page(params[:page]).per(10)
+    @top_articles = articles.first(20)
   end
 
   def search
