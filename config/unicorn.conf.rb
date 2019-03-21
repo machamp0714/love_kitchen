190391802332ac524cfd1202adf56dc3b71b6eff
25 macamp <rateliasudaisuki@gmail.com>
2019-03-21 14:46:05 +0900

    :up: unicornの設定ファイルを作成

---
+++ config/unicorn.conf.rb
@@ -0,0 +1,32 @@
+# set lets
+$worker  = 2
+$timeout = 30
+$app_dir = "/var/www/rails/love_kitchen" 
+$listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
+$pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
+$std_log = File.expand_path 'log/unicorn.log', $app_dir
+# set config
+worker_processes  $worker
+working_directory $app_dir
+stderr_path $std_log
+stdout_path $std_log
+timeout $timeout
+listen  $listen
+pid $pid
+# loading booster
+preload_app true
+# before starting processes
+before_fork do |server, worker|
+    defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+    old_pid = "#{server.config[:pid]}.oldbin"
+    if old_pid != server.pid
+        begin
+            Process.kill "QUIT", File.read(old_pid).to_i
+        rescue Errno::ENOENT, Errno::ESRCH
+        end
+    end
+end
+# after finishing processes
+after_fork do |server, worker|
+    defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
