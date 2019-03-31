317d6ac6949bf87ae51e5c2acc48cb4d1720470f
25 macamp <rateliasudaisuki@gmail.com>
2019-03-31 15:46:57 +0900

    :bug: app_dirを変更

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,7 +1,7 @@
 # set lets
 $worker  = 2
 $timeout = 30
-$app_dir = "/var/www/projects/love_kitchen/current" 
+$app_dir = "/var/www/projects/current" 
 $listen  = "/var/socks/.unicorn.sock"
 $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
 $std_log = File.expand_path 'log/unicorn.log', $app_dir
