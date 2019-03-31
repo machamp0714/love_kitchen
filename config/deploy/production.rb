e48c146e36b37fa0dbd3ec9e7b4f94a1acb15c82
25 macamp <rateliasudaisuki@gmail.com>
2019-03-31 10:49:26 +0900

    :rocket: cap:production.rbを編集
    デプロイするサーバーの情報を追加

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -41,17 +41,11 @@
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+set :ssh_options, keys: '~/.ssh/lovekitchen_key_rsa'
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-# server "example.com",
-#   user: "user_name",
-#   roles: %w{web app},
+server 'lovekitchen.cf', user: 'tatsuya', roles: %w{app web db}
 #   ssh_options: {
 #     user: "user_name", # overrides user setting above
 #     keys: %w(/home/user_name/.ssh/id_rsa),
