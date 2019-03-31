7b3990f6098286ec421d6180479a4f3003ac0252
25 macamp <rateliasudaisuki@gmail.com>
2019-03-31 11:58:28 +0900

    :rocket: デプロイ設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "love_kitchen"
+set :repo_url, "git@github.com:machamp0714/love_kitchen.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/var/www/projects"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -21,10 +21,10 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+set :linked_files, fetch(:linked_files, []).push('config/master.key')
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -33,7 +33,34 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
+set :rbenv_ruby, '2.5.3'
+
+set :log_level, :debug
+
+namespace :deploy do
+    desc 'Restart application'
+    task :restart do
+        invoke 'unicorn:restart'
+    end
+    desc 'Create database'
+    task :db_create do
+        on roles(:db) do |host|
+            with rails_env: fetch(:rails_env) do
+                within current_path do
+                    execute :bundle, :exec, :rake, 'db:create'
+                end
+            end
+        end
+    end
+
+    after :publishing, :restart
+    
+    after :restart, :clear_cache do
+        on roles(:web), in: :groups, limit: 3, wait: 10 do
+        end
+    end
+end
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
