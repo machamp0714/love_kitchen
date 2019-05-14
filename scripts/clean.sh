e053ee7166d1b2a2e5cf31367f25e17907d925c4
25 macamp <rateliasudaisuki@gmail.com>
2019-05-14 23:07:45 +0900

    :sparkles: fogをインストール #75

--- scripts/clean.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo rm -rf /var/www/projects/love_kitchen
--- scripts/start_server.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-su -l tatsuya -c 'cd /var/www/projects/love_kitchen && bundle exec unicorn -c /var/www/projects/love_kitchen/config/unicorn.conf.rb -D -E production'
