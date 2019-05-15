93d71bb9e96c1385480cbbdafe84cc8a4afa1e0b
25 macamp <rateliasudaisuki@gmail.com>
2019-05-16 00:01:30 +0900

    :bug: carrierwaveの設定変更 #75

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,13 @@
-Carrierwave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV["CHIRORU_ACCESS_ID"],
-    aws_secret_access_key: ENV["CHIRORU_SECRET_KEY"],
-    region: ENV["CHIRORU_REGION"]
-  }
+if Rails.env.production?
+  Carrierwave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV["CHIRORU_ACCESS_ID"],
+      aws_secret_access_key: ENV["CHIRORU_SECRET_KEY"],
+      region: ENV["CHIRORU_REGION"]
+    }
 
-  config.fog_directory = 'lovekitchen-bucket'
-  config.cache_storage = :fog
+    config.fog_directory = 'lovekitchen-bucket'
+  end
 end
