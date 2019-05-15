8612c9b387cb3f8e52d8286ff69a16fb7a2b0646
25 macamp <rateliasudaisuki@gmail.com>
2019-05-16 00:50:55 +0900

    :tada: carrierwaveの設定ファイルのスペルミスを修正 #75
    ローカルでの動作確認

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,11 @@
-if Rails.env.production?
-  Carrierwave.configure do |config|
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV["CHIRORU_ACCESS_ID"],
-      aws_secret_access_key: ENV["CHIRORU_SECRET_KEY"],
-      region: ENV["CHIRORU_REGION"]
-    }
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['CHIRORU_ACCESS_ID'],
+    aws_secret_access_key: ENV['CHIRORU_SECRET_KEY'],
+    region: ENV['CHIRORU_REGION']
+  }
 
-    config.fog_directory = 'lovekitchen-bucket'
-  end
+  config.fog_directory = 'lovekitchen-bucket'
 end
