4b1352ce0c0de6075bc41efd0bed8e0d6689d9c3
25 macamp <rateliasudaisuki@gmail.com>
2019-06-20 22:37:46 +0900

    :shower: migrationを追加 #87
    contributor_idをgood_user_idへrename

---
+++ db/migrate/20190620133439_rename_contributor_id_to_good_user_id.rb
@@ -0,0 +1,5 @@
+class RenameContributorIdToGoodUserId < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :notifications, :contributor_id, :good_user_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_20_123019) do
+ActiveRecord::Schema.define(version: 2019_06_20_133439) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -74,7 +74,7 @@ ActiveRecord::Schema.define(version: 2019_06_20_123019) do
 
   create_table "notifications", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "article_id"
-    t.integer "contributor_id"
+    t.integer "good_user_id"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
