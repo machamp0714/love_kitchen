484480cb5b462c5040660a73780f2f7e4269dae3
25 macamp <rateliasudaisuki@gmail.com>
2019-06-20 22:51:43 +0900

    :sparkles: alreadyカラムをnotificationsに追加 #87

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_20_133439) do
+ActiveRecord::Schema.define(version: 2019_06_20_134714) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 2019_06_20_133439) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "already", default: false
     t.index ["user_id"], name: "index_notifications_on_user_id"
   end
 
