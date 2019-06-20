4f627007f073d3b434f0556b4688b42f87506525
25 macamp <rateliasudaisuki@gmail.com>
2019-06-20 22:30:29 +0900

    :sparkles: likes_controllerを編集 #87
    いいねした時、通知も作成されるよう修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,6 +5,8 @@ class LikesController < ApplicationController
   def create
     current_user.likes.create(article_id: params[:article_id])
     @article = Article.find(params[:article_id])
+    @user = User.find(@article.user_id)
+    @user.notifications.create(article_id: @article.id, contributor_id: current_user.id)
     respond_to do |format|
       format.html { redirect_to @article }
       format.js
