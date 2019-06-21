c8cc41be6132f7d39fbf9091110cd31ff8b3de09
25 macamp <rateliasudaisuki@gmail.com>
2019-06-21 22:08:28 +0900

    :tada: 通知一覧viewを作成 #87

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,4 +4,16 @@ class Notification < ApplicationRecord
   def self.count_unread
     Notification.where(user_id: Current.user.id, already: false).count
   end
+
+  def article
+    Article.find_by(id: self.article_id)
+  end
+
+  def article_title
+    article.title
+  end
+
+  def user_name
+    User.find_by(id: self.like_user_id).name
+  end
 end
