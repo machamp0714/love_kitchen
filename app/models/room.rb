0a80f888e09e5fdf8e5ce5f794f0ecc343bbb725
25 macamp <rateliasudaisuki@gmail.com>
2019-06-30 21:00:03 +0900

    :sparkles: messageモデルとuser,roomモデルの間に1:nの関係を追加 #90

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,6 @@
 class Room < ApplicationRecord
   belongs_to :user
+  has_many :messages, dependent: :destroy
 
   validates :title, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,7 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   has_many :rooms, dependent: :destroy
+  has_many :messages, dependent: :destroy
 
   validates :name,
     presence: true,
