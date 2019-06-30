37ad8fdaf71c2dfda10deb81464af8b834413a78
25 macamp <rateliasudaisuki@gmail.com>
2019-06-30 14:32:19 +0900

    :sparkels: rooms_controllerへcreateアクションを追加 #90

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,18 @@ class RoomsController < ApplicationController
   def index
     @room = current_user.rooms.build
   end
+
+  def create
+    room = current_user.rooms.create(room_params)
+    if room.save
+      redirect_back(fallback_location: request.referer, notice: 'スレッドを作成しました。')
+    else
+      render :index
+    end
+  end
+
+  private
+    def room_params
+      params.require(:room).permit(:title)
+    end
 end
