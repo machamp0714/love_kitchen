2a413cafbe10416c9f374574fdcdec2d25273682
25 macamp <rateliasudaisuki@gmail.com>
2019-06-30 16:50:17 +0900

    :green_heart: スレッドを編集できることに対するテストを追加 #90

--- spec/features/rooms_spec.rb
+++ spec/features/rooms_spec.rb
@@ -2,18 +2,28 @@ require 'rails_helper'
 
 RSpec.feature "Rooms", type: :feature do
   given(:user) { FactoryBot.create(:alice) }
-  given(:room) { FactoryBot.build(:room) }
-
+  given!(:room) { FactoryBot.create(:room, user: user) }
+  
   scenario '新しくスレッドを作成できること', js: true do
     sign_in_as user
     visit rooms_path
     click_button 'スレッドを作成する'
     expect {
-        fill_in 'room[title]', with: room.title
+        fill_in 'room[title]', with: 'lovekitchen'
         click_on '作成'
     }.to change(Room, :count).by(1)
 
-    expect(page).to have_content room.title
+    expect(page).to have_content 'lovekitchen'
     expect(page).to have_content 'スレッドを作成しました。'
+  end
+
+  scenario 'スレッドを編集できること', js: true do
+    sign_in_as user
+    visit rooms_path
+    click_link '編集'
+    fill_in 'room[title]', with: 'edit title'
+    click_on '更新'
+    expect(page).to have_content 'edit title'
+    expect(page).to have_content 'タイトルを更新しました。'
   end
 end
