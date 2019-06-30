7ee6e5c65d9b914cdefbc6583688993f7b5a9bb8
25 macamp <rateliasudaisuki@gmail.com>
2019-06-30 14:23:22 +0900

    :green_heart: スレッドを作成できることに対するテストを追加 #90
    まだred

---
+++ spec/features/rooms_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature "Rooms", type: :feature do
+  given(:user) { FactoryBot.create(:alice) }
+  given(:room) { FactoryBot.build(:room) }
+
+  scenario '新しくスレッドを作成できること', js: true do
+    sign_in_as user
+    visit rooms_path
+    click_button 'スレッドを作成する'
+    expect {
+        fill_in 'rooms[title]', with: room.title
+        click_on '作成'
+    }.to change(Room, :count).by(1)
+
+    expect(page).to have_content room.title
+    expect(page).to have_content 'スレッドを作成しました。'
+  end
+end
