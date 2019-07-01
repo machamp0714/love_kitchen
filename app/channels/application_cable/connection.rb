819c20f3fa5d7caf7f9708be684eca7f2dc51c8a
25 macamp <rateliasudaisuki@gmail.com>
2019-07-02 00:23:32 +0900

    :sparkles: channelでcurrent_userを使えるようにする #90

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,20 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+      def find_verified_user
+        User.find(session['warden.user.user.key'][0][0])
+      rescue
+        reject_unauthorized_connection
+      end
+
+      def session
+        @session ||= cookies.encrypted[Rails.application.config.session_options[:key]]
+      end
   end
 end
