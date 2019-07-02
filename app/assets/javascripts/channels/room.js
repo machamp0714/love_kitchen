4251a817a2b706ebedbc4ae8944cbf07b794cafc
25 macamp <rateliasudaisuki@gmail.com>
2019-07-02 22:57:17 +0900

    :sparkles: room_channelにroom_idを渡せるように修正 #90

--- app/assets/javascripts/channels/room.js
+++ app/assets/javascripts/channels/room.js
@@ -1,23 +1,20 @@
-App.room = App.cable.subscriptions.create({ channel: "RoomChannel" }, {
-  connected: function() {
+jQuery(document).on('turbolinks:load', function() {
+  const messages = $('#messages');
 
-  },
+  if (messages.length > 0) {
+    App.room = App.cable.subscriptions.create({ channel: 'RoomChannel', room_id: messages.data('room_id') }, {
+      received: function(data) {
+        $('#messages').append(data['message']);
+      },
+      speak: function(message) {
+        return this.perform('speak', { message: message });
+      }
+    });
 
-  disconnected: function() {
-
-  },
-
-  received: function(data) {
-    alert(data['message'])
-  },
-
-  speak: function(message) {
-    return this.perform('speak', { message: message });
+    $(document).on('submit', '#messageForm', function () {
+      const input = document.getElementById('messageInput');
+      App.room.speak(input.value);
+      $('messageInput').value = '';
+    });
   }
-});
-
-$(document).on('submit', '#messageForm', function() {
-  const input = document.getElementById('messageInput');
-  App.room.speak(input.value);
-  input.value = '';
 });
