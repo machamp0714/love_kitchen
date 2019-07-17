449876c6792d5b9aad020e19b444e6e5b5c326f1
25 macamp <rateliasudaisuki@gmail.com>
2019-07-17 23:07:46 +0900

    :shirt: 記事詳細ページの写真表示部分のレイアウトを修正 #94

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -20,16 +20,16 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fill: [386, 246]
+  process resize_to_fill: [550, 400]
   #
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+    process resize_to_fill: [100, 80]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
