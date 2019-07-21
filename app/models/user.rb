dfb325dba2765304c5442d72105ed2e2647fa23c
25 macamp <rateliasudaisuki@gmail.com>
2019-07-21 22:23:23 +0900

    :+1: Userモデルのnameのformatを変更 #96
    nameのvalidationがきつすぎて、twitterのnameがvalidationにひっかっかってしまうのを防ぐため

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ApplicationRecord
   validates :name,
     presence: true,
     length: { in: 3..20 },
-    # format: { with: /\A(?!\d*\z)[a-z0-9]+\z/i }, # 半角英数字のみ ただし数字のみは不可
+    format: { with: /\A[\w@-]*[A-Za-z][\w@-]*\z/ }, # 半角英数字のみ ただし数字のみは不可
     uniqueness: { case_sensitive: false },
     ban_reserved: true
 
