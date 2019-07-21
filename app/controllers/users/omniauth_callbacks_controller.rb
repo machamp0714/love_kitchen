af82f909ca887084806e076818550ed0e524903f
25 macamp <rateliasudaisuki@gmail.com>
2019-07-21 15:24:53 +0900

    :sparkles: omniauth_callbacks_controllerにtwitterアクションを作成 #96

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,30 +1,19 @@
-# frozen_string_literal: true
-
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
+  def twitter
+    callback_from :twitter
+  end
 
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
+  private
+    def callback_from(provider)
+      provider = provider.to_s
 
-  # More info at:
-  # https://github.com/plataformatec/devise#omniauth
+      @user = User.find_for_oauth(request.env['omniauth.auth'])
 
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
-  # protected
-
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
+      if @user.persisted?
+        flash[:notice] = 'ログインしました。'
+        redirect_to root_path
+      else
+        redirect_to signup_path
+      end
+    end
 end
