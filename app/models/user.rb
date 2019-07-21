262967ddba074009f028feb9ca4f875d06865429
25 macamp <rateliasudaisuki@gmail.com>
2019-07-21 15:13:58 +0900

    :sparkles: find_for_oauthメソッドを修正 #96
    auth.info.nameだとvalidateされてしまうので、auth.info.nicknameを取る様に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -63,7 +63,7 @@ class User < ApplicationRecord
         provider: auth.provider,
         email: User.dummy_email(auth),
         password: Devise.friendly_token[0, 20],
-        name: auth.info.name,
+        name: auth.info.nickname,
         avatar: auth.info.image
       )
     end
