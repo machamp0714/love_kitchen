be6fa4c8edecff1d1415cd102a8c9740e8ec4d22
25 macamp <rateliasudaisuki@gmail.com>
2019-07-21 11:34:45 +0900

    :sparkles: devise_configにomniauthを追加 #96

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable
 
   mount_uploader :avatar, AvatarUploader
 
