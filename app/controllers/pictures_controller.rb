b278f8e068f1451288ae126f61f7588a1cf4bd26
25 macamp <rateliasudaisuki@gmail.com>
2019-08-12 22:17:10 +0900

    :sparkles: pictures_controllerへdestroyアクションを追加 #99

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -3,5 +3,14 @@ class PicturesController < ApplicationController
   end
 
   def destroy
+    @picture = Picture.find(params[:id])
+    article = Article.find(@picture.article_id)
+    @pictures = article.pictures
+    @picture.destroy
+
+    respond_to do |format|
+      format.html { redirect_to pictures_article_url(article), notice: '写真は削除されました。' }
+      format.js
+    end
   end
 end
