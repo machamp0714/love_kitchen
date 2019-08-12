02377f6375be65e62f7d553e2bb7f11df811447c
25 macamp <rateliasudaisuki@gmail.com>
2019-08-12 16:47:28 +0900

    :shirt: グラフ編集画面のフォームの余白を調整 #99

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -40,13 +40,14 @@ class ArticlesController < ApplicationController
 
     def pictures
         @article = Article.find(params[:id])
+        @pictures = @article.pictures
     end
 
     def chart
         @article = Article.find(params[:id])
         labels = [@article.label1, @article.label2, @article.label3, @article.label4, @article.label5]
         data = [@article.data1, @article.data2, @article.data3, @article.data4, @article.data5]
-        
+
         gon.labels = labels.reject { |label| label.blank? }
         gon.data = data.reject { |data| data.blank? }
     end
