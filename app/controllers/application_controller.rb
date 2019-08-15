57ded7d72286fae0b4bb52ee9162c7c8b521c4db
25 macamp <rateliasudaisuki@gmail.com>
2019-08-15 17:08:04 +0900

    :recycle: correct_userメソッドをapplication_controllerから削除 #101
    article_controllerでしか使わないのでメソッドを移動した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,11 +10,6 @@ class ApplicationController < ActionController::Base
             redirect_to @article if current_user.id == @article.user_id
         end
 
-        def correct_user
-            @article = current_user.articles.find_by(id: params[:id])
-            redirect_to root_url if @article.nil?
-        end
-
         def set_search
             @q = Article.ransack(params[:q])
             @search_articles = @q.result.includes(:user)
