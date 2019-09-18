c1081de7dd3ee2770d7808052bd465138bcdca6e
25 macamp <rateliasudaisuki@gmail.com>
2019-09-18 14:15:03 +0900

    :shirt: 検索フォームが空の時submit出来ないように修正 #104

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,5 +1,7 @@
 const likeCheckBox = document.getElementById("like_count");
 const viewCheckBox = document.getElementById("view_count");
+const searchForm = document.querySelector(".search-form");
+const searchInput = document.querySelector(".search-input");
 
 likeCheckBox.addEventListener("change", (event) => {
   if (event.target.checked && viewCheckBox.checked) {
@@ -10,5 +12,12 @@ likeCheckBox.addEventListener("change", (event) => {
 viewCheckBox.addEventListener("change", (event) => {
   if (event.target.checked && likeCheckBox.checked) {
     likeCheckBox.checked = false;
+  }
+});
+
+searchForm.addEventListener("submit", (event) => {
+  if (searchInput.value === "") {
+    event.preventDefault();
+    return false;
   }
 });
