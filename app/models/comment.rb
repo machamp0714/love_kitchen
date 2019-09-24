4e879ba1b75d1a6a40285c67ed4889dfaa3409e4
25 macamp <rateliasudaisuki@gmail.com>
2019-09-24 16:47:24 +0900

    :+1: comment, favorite, likeモデルにコールバックを追加 #103

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
 class Comment < ApplicationRecord
+  after_create_commit :create_notification
+
   belongs_to :article
   belongs_to :user
 
   validates :content, presence: true
+
+  private
+    def create_notification
+      user = self.article.user
+      user.increment!(:unread)
+      Notification.create(content: "#{self.user.name}さんが#{self.article.title}にコメントしました。", user_id: user.id)
+    end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 class Like < ApplicationRecord
+    after_create_commit :create_notification
+
     belongs_to :user
     belongs_to :article
+
+    private
+        def create_notification
+            user = self.article.user
+            user.increment!(:unread)
+            Notification.create(content: "#{self.user.name}さんが#{self.article.title}をいいねしました。", user_id: user.id)
+        end
 end
