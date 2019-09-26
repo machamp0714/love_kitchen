2d3fa702f1fd6c89a708a6da12d9f106f3069baf
25 macamp <rateliasudaisuki@gmail.com>
2019-09-26 16:45:27 +0900

    :shirt: 通知一覧ページにページネーションを追加 #103

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,7 +3,7 @@
 class NotificationsController < ApplicationController
   def index
     current_user.update(unread: 0)
-    @notifications = current_user.notifications.order(created_at: :desc)
+    @notifications = current_user.notifications.order(created_at: :desc).page(params[:page]).per(10)
     @notification_count = current_user.notifications.count
   end
 end
