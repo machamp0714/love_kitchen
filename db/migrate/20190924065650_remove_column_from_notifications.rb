fac1d10b62bfd42a2694ca4e4a039d4fc7d00115
25 macamp <rateliasudaisuki@gmail.com>
2019-09-26 17:18:49 +0900

    :recycle: rubocopによるコードの整形 #103

--- db/migrate/20190924065650_remove_column_from_notifications.rb
+++ db/migrate/20190924065650_remove_column_from_notifications.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RemoveColumnFromNotifications < ActiveRecord::Migration[5.2]
   def change
     remove_column :notifications, :article_id, :integer
--- db/migrate/20190924074903_add_article_id_to_notifications.rb
+++ db/migrate/20190924074903_add_article_id_to_notifications.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddArticleIdToNotifications < ActiveRecord::Migration[5.2]
   def change
     add_column :notifications, :article_id, :integer
